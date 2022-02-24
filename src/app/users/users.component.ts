acf77e2e78dd065fae7aaefbe2275745de460eb7
Zuzanka <hellingebleidd@gmail.com>
2022-02-24 16:03:25 +0100

    users

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/entities/user';
+import { UsersService } from 'src/services/users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users = [
+    new User("Zuzanka", "zuzanka@z.sk", 1, new Date("2022-02-24"), 'heslo'),
+    new User(" Anicka", "anicka@sk.sk", 2),
+    new User("Eliot", "-", 13),
+    new User("Eskel", "-", 4)
+  ]
+
+  selectedUser: User | undefined = undefined
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    // this.users = this.usersService.getLocalUsersSyn()
+    // this.usersService.getLocalUsers().subscribe(u => this.users = u)
+    this.usersService.getUsers().subscribe(u => this.users = u)
+  }
+
+  selectUser(user: User){
+    this.selectedUser = user;
+  }
+
+}
---
+++ src/services/users.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from 'src/entities/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private url = 'http://localhost:8080/'
+
+  //lokalni userovia
+  private users = [
+    new User("ZuzankaS", "zuzanka@z.sk", 1, new Date("2022-02-24"), 'heslo'),
+    new User(" AnickaS", "anicka@sk.sk", 2),
+    new User("EliotS", "-", 13),
+    new User("EskelS", "-", 4)
+  ]
+
+  constructor(private http: HttpClient) { }
+
+  //synchronne
+  public getLocalUsersSyn():User[]{
+    return this.users
+  }
+
+  //asynchronne
+  public getLocalUsers(): Observable<User[]>{
+    return of(this.users)
+  }
+
+  public getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.url+ 'users')
+  }
+}

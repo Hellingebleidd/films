cd4104b8baa3682feb73e1d4f624d824a720a1af
Zuzanka <hellingebleidd@gmail.com>
2022-03-15 11:57:55 +0100

    vlastne validatory

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import * as zxcvbn from 'zxcvbn';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   hide = true
 
+  passwordMessage = ''
+
+  passwordValidator = (control: AbstractControl): ValidationErrors | null => {
+    const result = zxcvbn(control.value)
+    const message = "password strength: "+result.score + "/4  --"+" can be guessed in "+result.crack_times_display.offline_slow_hashing_1e4_per_second
+    this.passwordMessage = message
+    return result.score < 3 ? {weakPassword: message} : null
+  }
+
   registerForm = new FormGroup({
     name: new FormControl('pstruhanka', [Validators.required, Validators.minLength(4)]),
     email: new FormControl('', [Validators.required, Validators.email, Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,}$")]),
-    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    password: new FormControl('', this.passwordValidator),
     password2: new FormControl(''),
-  })
+  }, this.passwordMatchValidator)
   constructor() { }
 
   ngOnInit(): void {
@@ -35,6 +45,19 @@ export class RegisterComponent implements OnInit {
 
   stringify(error:any): string{
     return JSON.stringify(error)
+  }
+
+  passwordMatchValidator(formModel: AbstractControl): ValidationErrors | null {
+    const pass1 = formModel.get('password')
+    const pass2 = formModel.get('password2')
+
+    if(pass1?.value === pass2?.value){
+      pass2?.setErrors(null)
+      return null
+    }
+    const err = {differentPasswd: "Passwords are not same"}
+    pass2?.setErrors(err)
+    return err
   }
 
   onSubmit(){

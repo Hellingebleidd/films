b6e6e9ff953a17176e4aeb8dc849b6f3a5770402
Zuzanka <hellingebleidd@gmail.com>
2022-03-31 12:42:32 +0200

    filter

--- src/modules/films/films-list/films-list.component.ts
+++ src/modules/films/films-list/films-list.component.ts
@@ -2,7 +2,7 @@ import { CollectionViewer, DataSource } from '@angular/cdk/collections';
 import { AfterViewInit, Component, EventEmitter, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { map, mergeAll, mergeMap, Observable, of, tap } from 'rxjs';
+import { map, mergeAll, mergeMap, Observable, of, Subject, tap } from 'rxjs';
 import { Film } from 'src/entities/film';
 import { FilmsResponse } from 'src/entities/films-response';
 import { FilmsService } from '../films.service';
@@ -19,6 +19,7 @@ export class FilmsListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined
   @ViewChild(MatSort) sort: MatSort | undefined
+  filter$ = new Subject<string>() //to je prud
 
   constructor(private filmsService: FilmsService) {
     this.filmsDataSource = new FilmsDataSource(filmsService)
@@ -36,12 +37,13 @@ export class FilmsListComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     if (this.paginator && this.sort)
-      this.filmsDataSource.addEventObservables(this.paginator, this.sort)
+      this.filmsDataSource.addEventObservables(this.paginator, this.sort, this.filter$.asObservable())
   }
 
   filter(event: any) {
-    const filter = event.target.value
+    const filterString = event.target.value
     //TODO spracovat
+    this.filter$.next(filterString)
   }
 
 }
@@ -52,14 +54,29 @@ class FilmsDataSource implements DataSource<Film>{
   paginator: MatPaginator | undefined
   indexFrom: number = 0
   indexTo: number = 10
+  filterString: string | undefined
 
   constructor(private filmsService: FilmsService) { }
 
-  addEventObservables(paginator: MatPaginator, sort: MatSort) {
+  goToFirstPage() {
+    if (this.paginator) {
+      this.indexFrom = 0
+      this.indexTo = this.paginator.pageSize
+      this.paginator.firstPage()
+    }
+  }
+
+  addEventObservables(paginator: MatPaginator, sort: MatSort, filter$: Observable<string>) {
     this.paginator = paginator
     this.indexFrom = 0
     this.indexTo = paginator.pageSize
 
+    this.futureObservables.emit(filter$.pipe(
+      tap(filterString => {
+        this.filterString = filterString.trim().toLowerCase() || undefined
+        this.goToFirstPage()
+      })
+    ))
     this.futureObservables.emit(of("init request"))
     this.futureObservables.emit(paginator.page.pipe(
       tap(pageEvent => {
@@ -76,7 +93,7 @@ class FilmsDataSource implements DataSource<Film>{
     return this.futureObservables.pipe(
       mergeAll(),
       tap(e => console.log("event to get files from server: ", e)),
-      mergeMap(e => this.filmsService.getFilms(this.indexFrom, this.indexTo)),
+      mergeMap(e => this.filmsService.getFilms(this.indexFrom, this.indexTo, this.filterString)),
       map((resp: FilmsResponse) => {
         if (this.paginator)
           this.paginator.length = resp.totalCount
